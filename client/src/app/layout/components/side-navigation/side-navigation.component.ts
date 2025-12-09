6df16c8bcc12dad85ace237ad603aa05106f9cb5
Jacob Staten <jscoding10@protonmail.com>
2025-12-08 16:25:39 -0800

    feat: add side navigation to layout

---
+++ client/src/app/layout/components/side-navigation/side-navigation.component.ts
@@ -0,0 +1,46 @@
+// Angular
+import { Component, signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+// PrimeNg
+import { TooltipModule } from 'primeng/tooltip';
+import { DividerModule } from 'primeng/divider';
+
+interface NavItem {
+  label: string;
+  icon: string;
+  route: string;
+}
+
+@Component({
+  selector: 'app-side-navigation',
+  imports: [RouterModule, TooltipModule, DividerModule],
+  templateUrl: './side-navigation.component.html',
+  styleUrl: './side-navigation.component.css',
+})
+export class SideNavigationComponent {
+  isExpanded = signal(false);
+
+  toggleSidebar() {
+    this.isExpanded.update((v) => !v);
+  }
+
+  navItems: NavItem[] = [
+    { label: 'Home', icon: 'home', route: '/home' },
+    {
+      label: 'Stock Research',
+      icon: 'trending_up',
+      route: '/stock-equity-research',
+    },
+    {
+      label: 'Crypto Research',
+      icon: 'currency_bitcoin',
+      route: '/crypto-equity-research',
+    },
+    {
+      label: 'Investment Property',
+      icon: 'apartment',
+      route: '/investment-property',
+    },
+  ];
+}
