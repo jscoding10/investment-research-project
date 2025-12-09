adf96e838468b7efc105a0fea65e4c5d1c84eb36
Jacob Staten <jscoding10@protonmail.com>
2025-12-08 18:45:07 -0800

    feat: add component logic to app

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+// Angular
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// Application
+import { TopBarComponent } from './layout/components/top-bar/top-bar.component';
+import { SideNavigationComponent } from './layout/components/side-navigation/side-navigation.component';
+import { FooterComponent } from './layout/components/footer/footer.component';
+import { MobileSideNavigationComponent } from './layout/components/mobile-side-navigation/mobile-side-navigation.component';
+
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [RouterOutlet, TopBarComponent, SideNavigationComponent, FooterComponent, MobileSideNavigationComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'client';
+  isMobileMenuOpen = signal(false);
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen.update((v) => !v);
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen.set(false);
+  }
 }
