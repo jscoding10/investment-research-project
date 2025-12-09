b15ca91693b969591e922cb161c297cd1ccd024f
Jacob Staten <jscoding10@protonmail.com>
2025-12-08 18:45:26 -0800

    feat: add providers to app config

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,8 +1,26 @@
+// Angular
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 
+// PrimeNg
+import Aura from '@primeuix/themes/aura';
+
+// Application
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+    provideRouter(routes),
+    provideHttpClient(),
+  ],
 };
