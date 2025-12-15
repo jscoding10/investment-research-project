be91efae3a8c0f2a6bc6ccde2a37577054641a71
Jacob Staten <jscoding10@protonmail.com>
2025-12-14 18:02:11 -0800

    fix: fix error with variable name

--- server/data/util/fetch_sec_filings.py
+++ server/data/util/fetch_sec_filings.py
@@ -13,10 +13,10 @@ from models.agent import FilingMetadata
 
 logger = get_logger(__name__)
 
-USER_AGENT = os.getenv("SEC_EDGAR_USER_AGENT")
+USER_AGENT = os.getenv("SEC_EDGAR_AGENT_KEY")
 if not USER_AGENT:
     raise ValueError(
-        "SEC_EDGAR_USER_AGENT environment variable must be set (format: 'youremail@domain.extension')"
+        "SEC_EDGAR_AGENT_KEY environment variable must be set (format: 'youremail@domain.extension')"
     )
 
 REQUEST_DELAY = 0.15  
