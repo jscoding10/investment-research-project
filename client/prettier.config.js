1b94ffee889239021f6dfbf60b48099063a1dbb2
Jacob Staten <jscoding10@protonmail.com>
2025-12-14 17:50:34 -0800

    chore: remove commented out code

--- client/prettier.config.js
+++ client/prettier.config.js
@@ -11,7 +11,7 @@ module.exports = {
   bracketSameLine: true,
   arrowParens: 'always',
   singleAttributePerLine: true,
-  plugins: ['prettier-plugin-tailwindcss'], // optional but recommended
+  plugins: ['prettier-plugin-tailwindcss'],
   overrides: [
     {
       files: '*.html',
