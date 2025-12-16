07399cb8010c3c2c9d9568610cfde1663b9e4d20
Jacob Staten <jscoding10@protonmail.com>
2025-12-15 18:40:03 -0800

    refactor: move environments folder location

--- client/src/app/features/home/services/current-market-data.service.ts
+++ client/src/app/features/home/services/current-market-data.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { catchError, Observable, of } from 'rxjs';
-import { environment } from '../../../../../environments/environment';
+import { environment } from '../../../../environments/environment';
 
 export interface StockData {
   ticker: string;
