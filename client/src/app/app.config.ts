448834c0bcc65bc11b4236abab19c9b6211ae4a5
Jacob Staten <jscoding10@protonmail.com>
2025-12-15 17:42:21 -0800

    feat: provide markdown at app level

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,6 +3,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { provideMarkdown } from 'ngx-markdown';
 
 // PrimeNg
 import Aura from '@primeuix/themes/aura';
@@ -13,6 +14,7 @@ import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideMarkdown(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideAnimationsAsync(),
     providePrimeNG({
