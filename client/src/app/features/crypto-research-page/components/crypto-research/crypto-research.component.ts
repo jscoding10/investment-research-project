5786c6887c8c6f48356ec76a9d419883b40691a8
Jacob Staten <jscoding10@protonmail.com>
2026-03-01 12:19:53 -0800

    refactor: update imports and add lifecycle hook to class

--- client/src/app/features/crypto-research-page/components/crypto-research/crypto-research.component.ts
+++ client/src/app/features/crypto-research-page/components/crypto-research/crypto-research.component.ts
@@ -1,5 +1,8 @@
-import { Component, inject, Input, signal } from '@angular/core';
+// Angular
+import { Component, inject, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// Application
 import { CryptoResearchFormComponent } from '../crypto-research-form/crypto-research-form.component';
 import { CryptoResearchReportComponent } from '../crypto-research-report/crypto-research-report.component';
 import { CryptoResearchStateService } from '../../services/crypto-research-state.service';
@@ -21,7 +24,7 @@ export interface CryptoReport {
   templateUrl: './crypto-research.component.html',
   styleUrl: './crypto-research.component.css',
 })
-export class CryptoResearchComponent {
+export class CryptoResearchComponent implements OnDestroy {
   state = inject(CryptoResearchStateService);
   report = this.state.report;
   isLoading = this.state.isLoading;
